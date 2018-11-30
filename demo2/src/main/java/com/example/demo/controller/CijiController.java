896e335491097b24c5d45387523e52639702780c
skycheng <sky.cheng@harman.com>
2018-11-30 16:08:17 +0800

    add delete edit

--- demo2/src/main/java/com/example/demo/controller/CijiController.java
+++ demo2/src/main/java/com/example/demo/controller/CijiController.java
@@ -3,8 +3,10 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,31 @@ public class CijiController {
 		}
 		return repository.findByPartNumOrPackage(p);
 
+	}
+
+	@RequestMapping("/toSecondaryAddEdit")
+	public String toEdit(Model model, int id) {
+		Ciji ciji = null;
+		if (id > 0) {
+			ciji = repository.getOne(id);
+		} else {
+			ciji = new Ciji();
+		}
+
+		model.addAttribute("ciji", ciji);
+		return "toSecondaryAddEdit";
+	}
+
+	@RequestMapping("/secondaryAddEdit")
+	public String secondaryAddEdit(Ciji ciji) {
+		repository.save(ciji);
+		return "redirect:/secondaryAdmin";
+	}
+
+	@RequestMapping("/toDelete")
+	public String delete(int id) {
+		repository.deleteById(id);
+		return "redirect:/secondaryAdmin";
 	}
 
 }
